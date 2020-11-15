2a905820cb3d1b37d8b52d3bf6fee426f241763c
Christos Choutouridis <cchoutou@ece.auth.gr>
2020-11-15 12:18:11 +0200

    typo and new line at the end of docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 # first arg is `-f` or `--some-option`
 # or there are no args
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-	# docker run bash -c 'echo hi'
+	# docker run hpcimage -c 'echo hi'
 	exec bash "$@"
 fi
 
